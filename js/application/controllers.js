2188a6b9663680c104ff0d654e0d792deecfc2fa
Peto <peterkapusta22@gmail.com>
2015-01-25 19:42:50 +0100

    oprava niektorych chyb vratane neodosielania kontaktneho formulara pridavanie properties lokality na jej podstranku

--- js/application/controllers.js
+++ js/application/controllers.js
@@ -42,6 +42,66 @@ controllers.controller('CountyLocationsController', ['$scope', '$routeParams', '
             $scope.rows = Helper.numberVal(locationsListRows);
         });
 
+        $scope.filter = true;
+        $scope.lengthFilter = false;
+        $scope.countyFilter = false;
+
+        $scope.counties = Counties.getCounties();
+
+        var lengthFrom = 1500;
+        var lengthTo = 60000;
+        $scope.length = {
+            min: lengthFrom,
+            max: lengthTo,
+            range: {
+                min: 0,
+                max: 200000,
+            }
+        };
+
+        $scope.filterLength = function () {
+            return function (location) {
+                if (location.length > $scope.length.min && location.length < $scope.length.max) {
+                    return location;
+                }
+                return null;
+            };
+        };
+
+        $scope.changeColor = function (id, color) {
+            var select = $("#" + id);
+            select.attr('style', 'color:' + color + ' !important');
+        };
+
+        $scope.toggleFilter = function (type) {
+            if (type === 'county') {
+                $scope.countyFilter = !$scope.countyFilter;
+                if (!$scope.countyFilter) {
+                    $scope.search.county_name = '';
+                } else {
+                    $scope.changeColor('selectCountyFilter', 'rgb(160, 160, 160)');
+                }
+            } else if (type === 'difficulty') {
+                $scope.difficultyFilter = !$scope.difficultyFilter;
+                if (!$scope.difficultyFilter) {
+                    $scope.search.difficulty = '';
+                } else {
+                    $scope.changeColor('selectDiffFilter', 'rgb(160, 160, 160)');
+                }
+            } else if (type === 'length') {
+                $scope.lengthFilter = !$scope.lengthFilter;
+                if (!$scope.lengthFilter) {
+                    //$scope.search.difficulty = '';
+                    $scope.length.min = lengthFrom;
+                    $scope.length.max = lengthTo;
+
+                } else {
+                    //$scope.changeColor('selectDiffFilter', 'rgb(160, 160, 160)');
+                }
+            }
+            ;
+        };
+
     }]);
 
 
@@ -49,10 +109,23 @@ controllers.controller('LocationController', ['$scope', '$routeParams', '$sce', 
     function ($scope, $routeParams, $sce, Locations, Helper) {
         window.scrollTo(0, 0);
 
+
+
         Locations.get({id: $routeParams.alias}, function (data) {
             $scope.location = data;
             var map = $scope.location.map;
             $scope.map = $sce.trustAsHtml(map);
+
+            $scope.difficulty = function () {
+                switch ($scope.location.difficulty) {
+                    case "easy":
+                        return {name: "Ľahká", style: $scope.location.difficulty};
+                    case "medium":
+                        return {name: "Stredne ťažká", style: $scope.location.difficulty};
+                    default:
+                        return {name: "Ťažká", style: $scope.location.difficulty};
+                }
+            }
 
             var count = parseInt($scope.location.images);
             $scope.countOfImages = Helper.numberVal(count);
@@ -77,18 +150,16 @@ controllers.controller('SearchForLocationController', ['$scope', '$routeParams',
         $scope.lengthFilter = false;
         $scope.countyFilter = false;
 
-        var lengthFrom = 1000;
-        var lengthTo = 7000;
-
         $scope.counties = Counties.getCounties();
 
-
+        var lengthFrom = 0;
+        var lengthTo = 100000;
         $scope.length = {
             min: lengthFrom,
             max: lengthTo,
             range: {
-                min: 500,
-                max: 10000
+                min: 0,
+                max: 100000
             }
         };
 
@@ -138,6 +209,7 @@ controllers.controller('SearchForLocationController', ['$scope', '$routeParams',
         Locations.query(function (data) {
             $scope.locations = data;
         });
+
     }]);
 
 
--- js/main.js
+++ js/main.js
@@ -2,18 +2,18 @@
 // totop
 // ***********************************
 
-$(function(){
-	$().UItoTop({ easingType: 'easeOutQuart' });
-});	
+$(function () {
+    $().UItoTop({easingType: 'easeOutQuart'});
+});
 
 
 // ***********************************
 // pageloader
 // ***********************************
-$(window).load(function(){
-	if($(".preloader").length > 0){
-		$('.preloader').fadeOut(1000); // set duration in brackets
-	}
+$(window).load(function () {
+    if ($(".preloader").length > 0) {
+        $('.preloader').fadeOut(1000); // set duration in brackets
+    }
 });
 
 
@@ -21,40 +21,73 @@ $(window).load(function(){
 // stop video when modal close
 // ***********************************
 
-$(function(){
-  $("#video-modal").on('hide.bs.modal', function(evt){
-    var player = $(evt.target).find('iframe'),
-        vidSrc = player.prop('src');
-    player.prop('src', ''); // to force it to pause
-    player.prop('src', vidSrc);
-  });
+$(function () {
+    $("#video-modal").on('hide.bs.modal', function (evt) {
+        var player = $(evt.target).find('iframe'),
+                vidSrc = player.prop('src');
+        player.prop('src', ''); // to force it to pause
+        player.prop('src', vidSrc);
+    });
 });
 
-	
+
 // ***********************************
 // Backstretch - Slider on Background
 //
 //  Note :  make sure  use this  http://bootstrapwizard.info/Theme/Fullscreen/images/bg4.jpg"  when on server  but you can simply use  "images/bg1.jpg"  if you are on localhost
 //
 // ***********************************								  
-			 
-$("body.bg-slider").backstretch([
-   "images/bg_1-velky.jpg",
-], {duration: 5000, fade: 1000});
-							  
+
+
+// Shuffles array elements
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+// Randomizes background images.
+function randomBgImages() {
+    var backgroundImages = [
+        "images/bg/0.jpg",
+        "images/bg/1.jpg",
+        "images/bg/2.jpg",
+        "images/bg/3.jpg",
+        "images/bg/4.jpg",
+    ];
+    
+    return shuffle(backgroundImages);
+}
+
+
+$("body.bg-slider").backstretch(randomBgImages(), {duration: 10000, fade: 1000});
+
 
 
 // ****************************************************************
 // counterUp
 // ****************************************************************
 
-$(function( $ ) {
-	if($("span.count").length > 0){	
-		$('span.count').counterUp({
-			delay: 10, // the delay time in ms
-			time: 1000 // the speed time in ms
-		});
-	}
+$(function ($) {
+    if ($("span.count").length > 0) {
+        $('span.count').counterUp({
+            delay: 10, // the delay time in ms
+            time: 1000 // the speed time in ms
+        });
+    }
 });
 
 
